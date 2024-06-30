08fe263c3d1d8281ff84e74ad1ae8c066750e448
dapraws <darrelprawira26@gmail.com>
2024-07-01 02:54:12 +0700

    feat: users who have already logged in don't need to log in again

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,21 +1,32 @@
 import 'package:drivepulse/app/routes/app_pages.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
+import 'package:nb_utils/nb_utils.dart';
 import '../controllers/splash_controller.dart';
 
 class SplashView extends GetView<SplashController> {
   const SplashView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 2), (() {
-      Get.offAllNamed(Routes.AUTH);
-    }));
+    _checkAuthentication();
     return Scaffold(
       body: Center(
         child: Image.asset("assets/images/logo-a.png"),
       ),
     );
+  }
+
+  void _checkAuthentication() async {
+    await Future.delayed(const Duration(seconds: 2));
+    User? user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      Get.offAllNamed(Routes.MAIN); // Navigate to main screen if logged in
+      toast("Welcome back, ${user.displayName}!");
+    } else {
+      Get.offAllNamed(Routes.AUTH); // Navigate to login screen if not logged in
+    }
   }
 }
